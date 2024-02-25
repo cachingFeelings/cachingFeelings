84cda440e2d27cb27ffc3a9257c10e79c5adfdc5
dingyuanzhang <zhangd7@myumanitoba.ca>
2024-02-25 16:34:17 -0600

    add function when there is no token in local storage, redirect to login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import Container from './components/landingpage/LandingPage'
-import { Routes, Route } from 'react-router-dom'
-import LoginForm from './components/landingpage/LoginForm'
-import SignupForm from './components/landingpage/SignupForm'
-import { SignUpProvider } from './context/SignUpContext'
-import  Try  from './components/homepage/try/Try'
-import  UserConfig from './components/homepage/user.config/userconfig'
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Container from './components/landingpage/LandingPage';
+import LoginForm from './components/landingpage/LoginForm';
+import SignupForm from './components/landingpage/SignupForm';
+import { SignUpProvider } from './context/SignUpContext';
+import Try from './components/homepage/try/Try';
+import UserConfig from './components/homepage/user.config/userconfig';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      navigate('/login');
+    }
+  }, [navigate]);
+
   return (
     <Routes>
       <Route path="/" element={<Container />} />
@@ -16,7 +25,7 @@ function App() {
       <Route path="/try" element={<Try />} />
       <Route path="/userconfig" element={<UserConfig />} />
     </Routes>
-  ); 
+  );
 }
 
 export default App;
