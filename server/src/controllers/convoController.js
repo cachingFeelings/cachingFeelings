f8d8032f9197d2fbfa5678e4f3114801d70b0d1d
bre9425 <breannabrown9425@gmail.com>
2024-03-09 16:19:22 -0600

    update getConvo to send array of convo IDs and recipients

--- server/src/controllers/convoController.js
+++ server/src/controllers/convoController.js
@@ -8,16 +8,20 @@ import User from '../models/userModel.js';
 //then return this as a json body similar to how batchGetMessages sends it
 export async function getConvo(req, res){
     try{
-        const convoID = req.body.convoID;
+        const userID = req.user; 
         
-        const convo = await Convo.findOne({_id: convoID});
+        const convo = await Convo.find(
+            { users: { $in: [userID] } },
+            { _id: 1, users: { $elemMatch: { $ne: userID } } }
+            );
 
-        const unauthorized = !convo.users.includes(req.user._id);
-        if(unauthorized){
-            res.status(401).send({message: "Accessing Unauthorized Resources"});
-        } else {
-            res.status(201).send({ convo });
+        if (!convo) {
+            res.status(404).send({ message: "No conversation found for the user." });
+            return;
         }
+
+        res.status(200).json(convo); 
+
     } catch (error) {
         if (error.kind == 'ObjectId'){
             res.status(404).send({message: "There's no Conversation ID to see here"});
@@ -26,6 +30,28 @@ export async function getConvo(req, res){
         }
     }
 }
+
+//OLD GETCONVO FUNCTION:
+// export async function getConvo(req, res){
+//     try{
+//         const convoID = req.body.convoID;
+        
+//         const convo = await Convo.findOne({_id: convoID});
+
+//         const unauthorized = !convo.users.includes(req.user._id);
+//         if(unauthorized){
+//             res.status(401).send({message: "Accessing Unauthorized Resources"});
+//         } else {
+//             res.status(201).send({ convo });
+//         }
+//     } catch (error) {
+//         if (error.kind == 'ObjectId'){
+//             res.status(404).send({message: "There's no Conversation ID to see here"});
+//         }else {
+//             res.status(400).send({ message: error.message });
+//         }
+//     }
+// }
 
 /*
 post /createConvo
@@ -44,6 +70,13 @@ export async function createConvo(req, res){
         
         if (!user){
             return res.status(404).send({message: `Who (${userID}) you tryna contact? ${recipient} The wind?`})
+        }
+
+        const existingConvo = await Convo.findOne({
+            users: { $all: [userID, recipient] }
+        });
+        if (existingConvo) {
+            return res.status(400).send({ message: "Conversation already exists for these users." });
         }
 
         let convoInfo = {
