e871b207e95136db25742f61a966cfa5d35c0140
dingyuanzhang <zhangd7@myumanitoba.ca>
2024-03-11 03:31:43 -0500

    add community router

--- server/src/routes/communityRouter.js
+++ server/src/routes/communityRouter.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { getPosts, newPosts } from '../controllers/communityController.js';
+import { getPosts, newPosts, likeDislikePosts, reportPosts, deletePosts } from '../controllers/communityController.js';
 import { decodeJWT } from '../config/auth.js'
 
 const router = express.Router();
 
 router.get('/getPosts', decodeJWT, getPosts);
 router.post('/newPosts', decodeJWT, newPosts);
+router.post('/likeDislike', decodeJWT, likeDislikePosts);
+router.post('/report', decodeJWT, reportPosts);
+router.delete('/delete', decodeJWT, deletePosts);
 
 export default router;
