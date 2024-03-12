4bb6b1cab551cf5b416c54acd4a6fceefe1643d4
Demi-AK <danielakinsola17@gmail.com>
2024-03-12 07:38:16 -0500

    Unit Testing

--- server/__test__/conversation.test.js
+++ server/__test__/conversation.test.js
@@ -1,33 +1,61 @@
 import request from 'supertest';
 import app from '../src/app.js';
 
-describe('Convo Router Tests', () => {
-    let token, token2, userID, userID2;
 
-    beforeAll(async () => {
-        // Create and log in two users to get their tokens and IDs for testing
-        let response = await request(app)
-            .post('/api/user/create_user')
+describe('POST /create_user', () => {
+    it('should create a new user and return a token', async () => {
+        const response = await request(app)
+            .post('/api/user/create_user') 
             .send({
                 data: {
-                    username: 'convoUser1',
-                    password: 'password1',
+                    username: 'test1User',
+                    password: 'test1Password',
                 }
             });
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toHaveProperty('token');
+    });
+
+    it('should create a new user and return a token', async () => {
+        const response = await request(app)
+            .post('/api/user/create_user') 
+            .send({
+                data: {
+                    username: 'test2User',
+                    password: 'test2Password',
+                }
+            });
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toHaveProperty('token');
+    });
+});
+
+
+describe('GET /getUser', () => {
+    let token;
+    let userID;
+    let token2;
+    let userID2;
+
+    beforeAll(async () => {
+        const response = await request(app)
+        .post('/api/user/login') 
+        .send({
+            username: 'test1User',
+            password: 'test1Password',
+        });
         token = response.body.token;
         userID = response.body.userObj._id;
 
-        response = await request(app)
-            .post('/api/user/create_user')
-            .send({
-                data: {
-                    username: 'convoUser2',
-                    password: 'password2',
-                }
-            });
-        token2 = response.body.token;
-        userID2 = response.body.userObj._id;
+        const respond = await request(app)
+        .post('/api/user/login') 
+        .send({
+            username: 'test2User',
+            password: 'test2Password',
+        });
 
+    token2 = respond.body.token;
+    userID2 = respond.body.userObj._id;
         // Make sure users like each other to enable conversation
         await request(app)
             .post('/api/user/likeDislike')
@@ -44,7 +72,7 @@ describe('Convo Router Tests', () => {
         const response = await request(app)
             .post('/api/convo/newConvo')
             .set('Authorization', `Bearer ${token}`)
-            .send({ username: 'convoUser2' }); // Assuming you need the username of the recipient to create a convo
+            .send({ username: 'test2User' }); // Assuming you need the username of the recipient to create a convo
 
         expect(response.statusCode).toBe(201);
     });
@@ -58,7 +86,7 @@ describe('Convo Router Tests', () => {
         expect(response.body).toEqual(expect.arrayContaining([
             expect.objectContaining({
                 _id: expect.any(String),
-                username: 'convoUser2'
+                username: 'Test2User'
             })
         ]));
     });
@@ -73,7 +101,7 @@ describe('Convo Router Tests', () => {
     it('should not allow unauthorized access to newConvo', async () => {
         const response = await request(app)
             .post('/api/convo/newConvo')
-            .send({ username: 'convoUser2' });
+            .send({ username: 'Test2User' });
 
         expect(response.statusCode).toBe(401);
     });
